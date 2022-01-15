a86ebd503c795c6916ae1e294ab7d1e4d7d2ee08
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-01-15 14:48:01 -0400

    chore(techstack-#2): basic ReactQuery setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+const queryClient = new QueryClient();
 
 /**
  * [App is the main component of the application]
@@ -9,22 +11,14 @@ import './App.css';
  */
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
+      <div className="App">
+        <header className="App-header">
+          <p>Hello VidIQ!</p>
+        </header>
+      </div>
+    </QueryClientProvider>
   );
 }
 
