b89769ec9db0c7cd2f5e7dc1bbbabc87912820ef
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-01-18 18:53:03 -0400

    feat: (test) basic App exists test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders the heading', () => {
   render(<App />);
   const linkElement = screen.getByText(/Hello VidIQ!/i);
   expect(linkElement).toBeInTheDocument();
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+
+import { Carousel } from './components/molecules';
 import GlobalStyle from './styles';
 
 const queryClient = new QueryClient();
@@ -15,11 +17,12 @@ function App(): JSX.Element {
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={true} />
       <GlobalStyle />
-      <div className="App">
+      <main className="App">
         <header className="text-3xl font-bold underline">
           <p>Hello VidIQ!</p>
         </header>
-      </div>
+        <Carousel />
+      </main>
     </QueryClientProvider>
   );
 }
