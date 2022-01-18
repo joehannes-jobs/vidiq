e8b762c2189fdc446d9eb5e8d5344418e569f875
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-01-18 18:38:26 -0400

    feat(tests): test for Carousel incl snapshot/layout-test and stub-data

---
+++ src/components/molecules/carousel/Carousel.test.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import nock from 'nock';
+import { shallow, mount } from 'enzyme';
+import { waitFor } from '@testing-library/react';
+
+import Carousel, { ImageAPIBaseUrl } from '.';
+import CarouselImage from './CarouselImage';
+import ImageData from './stub.json';
+
+// increase timeout: the big .json test is taking some time
+jest.setTimeout(21000);
+
+describe('Carousel', () => {
+  let fullyRenderedComponent: any;
+  let shallowRenderedComponent: any;
+  let scope: any = nock(ImageAPIBaseUrl)
+    .defaultReplyHeaders({
+      'access-control-allow-origin': '*',
+      'access-control-allow-credentials': 'true',
+    })
+    .persist();
+
+  beforeAll(() => {
+    scope.persist().get('/photos').reply(200, ImageData.slice(0, 10), {
+      'Content-Type': 'application/json',
+    });
+
+    shallowRenderedComponent = shallow(<Carousel />);
+    fullyRenderedComponent = mount(<Carousel />);
+  });
+
+  afterAll(() => {
+    scope.done();
+  });
+
+  it('should render the Carousel Component', () => {
+    expect(fullyRenderedComponent.equals(<Carousel />)).toEqual(true);
+  });
+
+  it('should render a single toplevel <section /> tag', () => {
+    expect(shallowRenderedComponent.find('section').length).toEqual(1);
+  });
+
+  it('should render initial Layout', () => {
+    expect(shallowRenderedComponent.getElements()).toMatchSnapshot();
+  });
+
+  it('should load data and render <CarouselImage />', async () => {
+    await waitFor(
+      () => {
+        fullyRenderedComponent.update();
+        expect(
+          fullyRenderedComponent.containsAnyMatchingElements(
+            ImageData.slice(0, 10).map(dataset => (
+              <CarouselImage
+                key={`album-${dataset.albumId}-img-${dataset.id}`}
+                albumId={dataset.albumId}
+                id={dataset.id}
+                title={dataset.title}
+                thumbnailUrl={dataset.thumbnailUrl}
+              />
+            ))
+          )
+        ).toEqual(true);
+      },
+      { interval: 50, timeout: 3000 }
+    );
+  });
+});
