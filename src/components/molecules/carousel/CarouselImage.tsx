cbb3cdbad2e4980c56b96c6ecd4415ea6b6c4173
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-01-19 01:09:37 -0400

    feat(pagination): infinite scrolling and data fetching

--- src/components/molecules/carousel/CarouselImage.tsx
+++ src/components/molecules/carousel/CarouselImage.tsx
@@ -1,21 +1,28 @@
-import React, { PropsWithChildren } from 'react';
+import React, { RefAttributes, PropsWithRef } from 'react';
+import styled from 'styled-components';
+
+const Thumbnail = styled.div`
+  width: 150px;
+  height: 150px;
+`;
 
 export interface CarouselImageProps {
-  albumId: number;
-  id: number;
   title: string;
   thumbnailUrl: string;
 }
 
-const CarouselImage: React.FC<CarouselImageProps> = ({
-  albumId,
-  id: imgId,
-  title,
-  thumbnailUrl: url,
-}: PropsWithChildren<CarouselImageProps>): JSX.Element => (
-  <div key={`album-${albumId}-img-${imgId}`} className={`carousel-item`}>
-    <img src={url} alt={title} />
-  </div>
+const CarouselImage: React.ForwardRefExoticComponent<
+  CarouselImageProps & RefAttributes<HTMLDivElement>
+> = React.forwardRef<HTMLDivElement, CarouselImageProps>(
+  (
+    { title, thumbnailUrl: url }: PropsWithRef<CarouselImageProps>,
+    ref: React.Ref<HTMLDivElement> = null
+  ): JSX.Element => (
+    <Thumbnail ref={ref ?? undefined} className={`carousel-item`}>
+      <img src={url} alt={title} />
+    </Thumbnail>
+  )
 );
+CarouselImage.displayName = 'CarouselImage';
 
 export default CarouselImage;
