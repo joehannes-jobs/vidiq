93430527ee53b47812b0d472075e0f44b67bdfbd
Johannes Neugschwentner <johannes.neugschwentner@gmail.com>
2022-01-25 00:38:21 -0400

    chore(structure): structural exports

--- src/components/molecules/carousel/index.tsx
+++ src/components/molecules/carousel/index.tsx
@@ -1,2 +1,5 @@
-export * from './Carousel';
-export { default } from './Carousel';
+export { default as Aside } from './Aside';
+export { default as Image } from './CarouselImage';
+export * as Context from './Context';
+export { API } from './Carousel';
+export { default as Component } from './Carousel';
---
+++ src/components/pages/index.tsx
@@ -0,0 +1,1 @@
+export { default as DrawerGallery } from './DrawerGallery';
